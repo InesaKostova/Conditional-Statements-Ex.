4656dd2b13caa8e76871d52af5be8ca461f1b2c9
Inesa Kostova <kostovainesa@gmail.com>
2025-02-12 17:48:32 +0200

    Add files via upload

---
+++ Sum of Seconds.cs
@@ -0,0 +1,17 @@
+﻿int first = int.Parse(Console.ReadLine());
+int second = int.Parse(Console.ReadLine());
+int third = int.Parse(Console.ReadLine());
+
+int totalTime = first + second + third;
+
+int minutes = totalTime / 60;
+int seconds = totalTime % 60;
+
+if ( seconds < 10)
+{
+    Console.WriteLine($"{minutes}:0{seconds}"); 
+}
+else
+{
+    Console.WriteLine($"{minutes}:{seconds}"  );
+}

71966b971999219e23dd28e4a46135c63cc62ea6
Inesa Kostova <kostovainesa@gmail.com>
2025-02-13 15:18:12 +0200

    Add files via upload

---
+++ Godzilla vs. Kong.cs
@@ -0,0 +1,23 @@
+﻿double bujet = double.Parse(Console.ReadLine());
+int countActors = int.Parse(Console.ReadLine());
+double priceForDresses = double.Parse(Console.ReadLine());
+
+double priceDecor = bujet * 0.10;
+double sumForAllDresses = countActors * priceForDresses;
+
+if ( countActors >150)
+{
+    sumForAllDresses = sumForAllDresses - (sumForAllDresses * 0.10);
+}
+
+double allFinalSum = priceDecor + sumForAllDresses;
+if ( allFinalSum >bujet )
+{
+    Console.WriteLine("Not enough money!");
+    Console.WriteLine($"Wingard needs {allFinalSum - bujet:F2} leva more.");
+}
+else
+{
+    Console.WriteLine("Action!");
+    Console.WriteLine($"Wingard starts filming with {bujet - allFinalSum:F2} leva left.");
+}
